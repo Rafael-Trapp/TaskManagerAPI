27e5e60dbd0f622c013f56ca7fe3f33ed511e3d3
Rafael-Trapp <rafatrapp99@gmail.com>
2026-06-25 12:04:58 -0400

    App context created

---
+++ TaskManagerAPI/Data/TaskDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task = TaskManagerAPI.Models.Task;
+
+namespace TaskManagerAPI.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) 
+            : base(options) { }
+    }
+}
---
+++ TaskManagerAPI/Models/TaskType.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TaskManagerAPI.Models;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum TaskType
+{
+    Work,
+    School,
+    Personal,
+    Shopping,
+    House,
+    Others
+}
